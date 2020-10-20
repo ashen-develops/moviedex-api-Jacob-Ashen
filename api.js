99d7add7256f27797b05b941515d01ba7d580744
Jacob Boodry <jake.boodry@live.com>
2020-10-20 13:53:18 -0700

    added node scripts

--- api.js
+++ api.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
-const games = require('./data.js');
+const data = require('./data.js');
 
 const app = express();
 
 app.use(morgan('dev'))
 app.get('/', (req, res) =>{
   res.send('Hello!')
+})
+
+app.get('/movie', (req, res) => {
+  const {genre, country, avg_vote} = req.query;
+  let movies = data;
+  console.log(movies)
+
+
+
 })
